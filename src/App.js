e5f10916805aad8b556ad6eb74d0046bd43437be
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-30 01:18:35 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
- 
+
 class App extends Component {
- 
+
   handleOnClick = event => {
     this.props.addItem() // Code change: this.props.dispatch.store is no longer being called
   }
- 
+
   render() {
     return (
       <div className="App">
@@ -20,13 +20,13 @@ class App extends Component {
     );
   }
 };
- 
+
 const mapStateToProps = (state) => {
   return {
     items: state.items
   };
 };
- 
+
 // Code change: this new function takes in dispatch as an argument
 // It then returns an object that contains a function as a value!
 // Notice above in handleOnClick() that this function, addItem(),
@@ -38,5 +38,5 @@ const mapDispatchToProps = dispatch => {
     }
   };
 };
- 
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
